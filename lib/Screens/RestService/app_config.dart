1250f66096ebb3d18d9d09a0e72c4bc08075cb76
Gayathri <gayathri.v@kanmalaitech.com>
2021-04-19 19:22:41 +0530

    ios package fix

--- lib/Screens/RestService/app_config.dart
+++ lib/Screens/RestService/app_config.dart
@@ -2,5 +2,6 @@ class Config {
 // get baseUrl =>
 //     "https://325p0kj62c.execute-api.us-east-2.amazonaws.com/churchapi";
   static const baseUrl =
-      "https://zrrlyf7lv2.execute-api.us-east-1.amazonaws.com/churchapi";
+      // "https://zrrlyf7lv2.execute-api.us-east-1.amazonaws.com/churchapi";
+  "https://325p0kj62c.execute-api.us-east-2.amazonaws.com/churchapi";
 }
